26544f1c65485c8df0454c2dcae678990dfc0b29
adamz <58021562+ad4mny@users.noreply.github.com>
2021-12-30 03:06:36 +0800

    init

---
+++ www/js/app.js
@@ -0,0 +1,10 @@
+var token = JSON.parse(localStorage.getItem('token'));
+// var url = 'http://localhost/scan-and-shop-system/';
+var url = 'https://scan-and-shop-system.000webhostapp.com/';
+
+var logout = function () {
+
+    localStorage.clear();
+    location.replace('index.html');
+
+};
